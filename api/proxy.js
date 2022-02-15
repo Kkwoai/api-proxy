2d0696bdc7e25900d5ea866fa906432a1bc56616
wang <470750572@qq.com>
2022-02-15 10:44:16 +0800

    test

--- api/proxy.js
+++ api/proxy.js
@@ -1,20 +1,16 @@
 const { createProxyMiddleware } = require('http-proxy-middleware')
+const path = require('path'); //系统路径模块
+const fs = require('fs'); //文件模块
 
 module.exports = (req, res) => {
-  let target = ''
+  let file = path.join(__dirname, 'version.json'); //文件路径，__dirname为当前运行js文件的目录
+  //读取json文件
+  fs.readFile(file, 'utf-8', function(err, data) {
+    if (err) {
+          res.send('文件读取失败');
+      } else {
+          res.json(data);
+      }
+  });
 
-  // 代理目标地址
-  // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
-  if (req.url.startsWith('/api')) {
-    target = 'https://api.music.xxytime.top'
-  }
-
-  // 创建代理对象并转发请求
-  createProxyMiddleware({
-    target,
-    changeOrigin: true,
-    pathRewrite: {
-      '^/api/': '/'
-    }
-  })(req, res)
 }
